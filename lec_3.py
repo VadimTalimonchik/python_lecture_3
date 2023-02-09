a11347bebd47675bcc4be1764ec3d23aed05eb56
VadimTalimonchik <tvadmins@gmail.com>
2023-02-09 15:04:13 +0300

    ФУНКЦИЯ filter

--- lec_3.py
+++ lec_3.py
@@ -173,3 +173,14 @@
 # res = list(map(lambda x: (x, x ** 2), res))
 # print(res)
 
+# ФУНКЦИЯ filter
+# data = [x for x in range(10)]
+# res = list(filter(lambda x: not x % 2, data))
+# print(res)
+
+# data = '1 2 3 5 8 15 23 38'.split()
+
+# res = map(int, data)
+# res = filter(lambda x: not x % 2, res)
+# res = list(map(lambda x: (x, x ** 2), res))
+# print(res)

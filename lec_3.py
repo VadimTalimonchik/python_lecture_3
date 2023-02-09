87a79dfce30de1ed452311a1800ee95638b620d5
VadimTalimonchik <tvadmins@gmail.com>
2023-02-09 15:23:25 +0300

    ФУНКЦИЯ zip

--- lec_3.py
+++ lec_3.py
@@ -184,3 +184,10 @@
 # res = filter(lambda x: not x % 2, res)
 # res = list(map(lambda x: (x, x ** 2), res))
 # print(res)
+
+# ФУНКЦИЯ zip
+# user = ['user1', 'user2', 'user3', 'user4', 'user5']
+# ids = [4, 5, 9, 14, 7]
+# salary = [111, 222, 333]
+# data = list(zip(user, ids, salary))
+# print(data)

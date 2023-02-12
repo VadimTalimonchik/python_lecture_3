3da06e3040ab8aa3ed002cf364d1fc87a884af1e
VadimTalimonchik <tvadmins@gmail.com>
2023-02-12 11:55:42 +0300

    АЛГОРИТМЫ Задача

--- lec_3_v2.py
+++ lec_3_v2.py
@@ -64,14 +64,49 @@
 #     list1.append(fib(i))
 # print(list1)
 
-# АЛГОРИТМЫ
-def quick_sort(array):
-    if len(array) <= 1:
-        return array
-    else:
-        pivot = array[0]
-    less = [i for i in array[1:] if i <= pivot]
-    greater = [i for i in array[1:] if i > pivot]
-    return quick_sort(less) + [pivot] + quick_sort(greater)
-# print(quick_sort([14, 5, 9, 6, 3, 5, 8, 7, 5, 2, 7]))
-print(quick_sort([10, 5, 2]))
+# # АЛГОРИТМЫ
+# def quick_sort(array):
+#     if len(array) <= 1:
+#         return array
+#     else:
+#         pivot = array[0]
+#     less = [i for i in array[1:] if i <= pivot]
+#     greater = [i for i in array[1:] if i > pivot]
+#     return quick_sort(less) + [pivot] + quick_sort(greater)
+# # print(quick_sort([14, 5, 9, 6, 3, 5, 8, 7, 5, 2, 7]))
+# print(quick_sort([10, 5, 2]))
+
+# ЗАДАЧА
+# Сортировка СЛИЯНИЕМ
+def merge_sort(nums):
+    if len(nums) > 1:
+        mid = len(nums) // 2
+        left = nums[:mid]
+        right = nums[mid:]
+        merge_sort(left)
+        merge_sort(right)
+        i = j = k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                nums[k] = left[i]
+                i += 1
+            else:
+                nums[k] = right[j]
+                j += 1
+            k += 1
+        
+        while i < len(left):
+            nums[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            nums[k] = right[j]
+            j += 1
+            k += 1
+
+list1 = [38, 27, 43, 3, 9, 82, 10]
+merge_sort(list1)
+print(list1)
+  
